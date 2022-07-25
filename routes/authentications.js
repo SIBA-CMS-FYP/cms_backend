083c03785a30aafcf1b0cdf5406dab0cc4749095
hansraj <hansraj.bcsf18@iba-suk.edu.pk>
2022-07-25 23:56:41 +0500

    take pull from git hans-f-e

--- routes/authentications.js
+++ routes/authentications.js
@@ -5,6 +5,7 @@ const route=express.Router()
 const con=require('./connection') 
 
 route.post("/login", (req , res)=>{
+<<<<<<< HEAD
 
     var cms=req.query.cms || req.body.cms || req.params.cms;
     var password=req.query.password || req.body.password || req.params.password;
@@ -26,6 +27,23 @@ route.post("/login", (req , res)=>{
     });
 
 // })
+=======
+    var cms=req.body.cms;
+    var password=req.body.password;
+    con.connect(function(err){
+        if(err){
+            console.log("ERROR "+err.message)
+            return;
+        }
+        con.query("SELECT * FROM authentication WHERE cms=? AND password=?;",[cms,password],(error, row, column)=>{
+        if(error)
+            return res.send("ERROR OCCURED")
+
+        res.send(JSON.stringify({succes:true,message:row}))
+    })         
+    })    
+})
+>>>>>>> 9c9e371 (move to hans-f-e branch)
 
 
 module.exports=route
