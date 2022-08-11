39dc81989d2c0f9aede89a573d58cbb5de22f210
hansraj <hansraj.bcsf18@iba-suk.edu.pk>
2022-08-11 14:45:10 +0500

    HOD authentication added

--- routes/teacher_authentications.js
+++ routes/teacher_authentications.js
@@ -7,11 +7,7 @@ route.post("/techerlogin", (req, res) => {
     var teacher_id =req.body.teacher_id;
     var password = req.body.password;
     console.log("teacher CMS "+teacher_id+" Password "+password)
-    // con.connect(function(err){
-    //     if(err){
-    //         console.log("ERROR "+err.message)
-    //         return;
-    //     }
+   
     con.query("SELECT * FROM techer_auth WHERE teacher_id=? AND password=?;", [teacher_id, password], (error, row, column) => {
 
         if (error)
@@ -25,6 +21,24 @@ route.post("/techerlogin", (req, res) => {
             res.send({success:false,message:"Invalide credential"})
         }
     })
+
+})
+route.post("/hodLogin", (req, res) => {
+    var hodcms =req.query.hodcms;
+    var password = req.query.password;
+    console.log("teacher CMS "+hodcms+" Password "+password)
+    con.query("SELECT * FROM hod_authentication WHERE hodcms=? AND password=?;", [hodcms, password], (error, row, column) => {
+        if (error)
+            console.log("ERROR OCCURED"+error.message);
+         console.log(row[0]);
+        if (row.length > 0) {
+            res.send({success:true,message:"login Successfull"})
+        }
+        else {
+            res.send({success:false,message:"Invalide credential"})
+        }
+    })
+    
 })
 // })
 
