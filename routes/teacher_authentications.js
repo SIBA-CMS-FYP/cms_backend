6b0d6c6048e65894b2960bfebd76ec79aa18a739
Suraksha <surakshak571@gmail.com>
2022-08-13 22:26:02 +0500

    backend for hod login

--- routes/teacher_authentications.js
+++ routes/teacher_authentications.js
@@ -7,11 +7,6 @@ route.post("/techerlogin", (req, res) => {
     var teacher_id =req.body.teacher_id;
     var password = req.body.password;
     console.log("teacher CMS "+teacher_id+" Password "+password)
-    // con.connect(function(err){
-    //     if(err){
-    //         console.log("ERROR "+err.message)
-    //         return;
-    //     }
     con.query("SELECT * FROM techer_auth WHERE teacher_id=? AND password=?;", [teacher_id, password], (error, row, column) => {
 
         if (error)
@@ -27,6 +22,25 @@ route.post("/techerlogin", (req, res) => {
     })
 })
 // })
+
+route.post("/hodlogin", (req, res) => {
+    var hodcms =req.body.hodcms;
+    var password = req.body.password;
+    console.log("hod CMS "+ hodcms+" Password "+password)
+    con.query("SELECT * FROM hod_authentication WHERE hodcms=? AND password=?;", [hodcms, password], (error, row, column) => {
+
+        if (error)
+            console.log("ERROR OCCURED");
+        console.log(row[0]);
+
+        if (row.length > 0) {
+            res.send({success:true,message:"login Successfull"})
+        }
+        else {
+            res.send({success:false,message:"Invalid credential"})
+        }
+    })
+})
 
 
 module.exports = route
